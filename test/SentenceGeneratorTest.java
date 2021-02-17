65b49609c8b7705af98ca3b13adc01646de5b7b7
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-02-17 18:41:05 +0100

    program generates words randomly to form sentences

---
+++ test/SentenceGeneratorTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SentenceGeneratorTest {
+    SentenceGenerator sentenceGenerator;
+
+    @BeforeEach
+    void setUp() {
+        sentenceGenerator = new SentenceGenerator();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getArticleArray(){
+        assertNotNull(sentenceGenerator.getArticle());
+        assertEquals(5,sentenceGenerator.getArticle().length);
+    }
+
+    @Test
+    void generateSentence(){
+        String sentence = sentenceGenerator.getSentence();
+        assertNotNull(sentence);
+        assertFalse(sentence.isEmpty());
+
+        for (int counter= 0; counter < 20; counter++){
+            System.out.println(sentenceGenerator.getSentence());
+        }
+    }
+
+}
